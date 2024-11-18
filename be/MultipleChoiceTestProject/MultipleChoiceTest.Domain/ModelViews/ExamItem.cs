acc4bca2e46a46dea2a68b6b4f77963957538329
hoantd <taduchoan5503@gmail.com>
2024-11-19 01:07:22 +0700

    update

---
+++ be/MultipleChoiceTestProject/MultipleChoiceTest.Domain/ModelViews/ExamItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MultipleChoiceTest.Domain.ModelViews
+{
+    public class ExamItem
+    {
+        public int Id { get; set; }
+        public string? LessonName { get; set; }
+        public int? SubjectId { get; set; }
+        public string? SubjectName { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool? IsDeleted { get; set; } = false;
+    }
+}
---
+++ be/MultipleChoiceTestProject/MultipleChoiceTest.Web/Areas/Admin/Controllers/ExamsController.cs
@@ -0,0 +1,59 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MultipleChoiceTest.Domain.Models;
+using MultipleChoiceTest.Domain.ModelViews;
+using MultipleChoiceTest.Web.Api;
+
+namespace MultipleChoiceTest.Web.Areas.Admin.Controllers
+{
+    public class ExamsController : BaseController
+    {
+        public ExamsController(INotyfService notyfService, IHttpContextAccessor httpContextAccessor, ILogger<BaseController> logger, IMapper mapper) : base(notyfService, httpContextAccessor, logger, mapper)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string searchKey = "")
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            await CreateViewBagAsync();
+            return View();
+        }
+
+        // GET: Brand/Edit/Id
+        public async Task<IActionResult> Edit(int id)
+        {
+            var detailRs = await ApiClient.GetAsync<Exam>(Request, $"Exams/{id}");
+            var data = _mapper.Map<CUExam>(detailRs.Data);
+            if (detailRs.Success)
+            {
+                await CreateViewBagAsync(data);
+                return View(data);
+            }
+
+            _notyfService.Error("Không tìm thấy môn học");
+            await CreateViewBagAsync(data);
+            return View(data);
+        }
+
+        private async Task CreateViewBagAsync(CUExam? exam = null)
+        {
+            var subjects = await ApiClient.GetAsync<List<Subject>>(Request, "Subjects");
+            if (exam != null)
+            {
+                ViewData["Subjects"] = new SelectList(subjects.Data, "Id", "SubjectName", exam.SubjectId);
+            }
+            else
+            {
+                ViewData["Subjects"] = new SelectList(subjects.Data, "Id", "SubjectName");
+            }
+        }
+    }
+}
